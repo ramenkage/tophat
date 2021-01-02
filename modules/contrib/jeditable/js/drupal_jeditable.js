476e29cec5ab296e06a5f1a21697aa3498f87248
Yoni Gray <ramenkage@gmail.com>
2021-01-01 19:39:06 -0500

    Tracking hacked file in contrib module

---
+++ modules/contrib/jeditable/js/drupal_jeditable.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Triggers editable function from min.js.
+ */
+
+(function ($) {
+    Drupal.behaviors.jeditable = {
+        attach: function (context) {
+            $('.jeditable-textfield', context).editable('/jeditable/ajax/save', {
+                indicator   : 'Saving...',
+                tooltip     : 'Click to edit...',
+                cancel      : 'Cancel',
+                submit      : 'Save',
+                placeholder : '&nbsp;'
+            });
+        }
+    };
+})(jQuery);
